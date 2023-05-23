f702f4ef9c9c8cf9c48e92876441cc9b83c5dcf8
Luis Eduardo M <luiseduardomc019@gmail.com>
2023-05-23 20:40:51 -0300

    name fix

--- Sorting/inserction.c
+++ Sorting/inserction.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void InsectionSort(int array[], int size)
+void InsertionSort(int array[], int size)
 {
     for (int i = 1; i < size; i++)
     {
@@ -20,7 +20,7 @@ int main()
     int array[5] = {9, 5, 7, 1, 2};
     int size = sizeof(array) / sizeof(array[0]);
 
-    InsectionSort(array, size);
+    InsertionSort(array, size);
 
     for (int i = 0; i < size; i++)
     {
